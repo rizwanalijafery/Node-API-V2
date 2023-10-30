1800aa00b60bf3b27ea3f85f9a6efc2882220ce0
rizwanalijafery <130446420+rizwanalijafery@users.noreply.github.com>
2023-10-31 02:19:13 +0500

    finsih node api version2

--- controller/productController.js
+++ controller/productController.js
@@ -1,63 +1,72 @@
 const Product = require("../models/productmodel");
-const getProducts = async (req,res)=>{
+const asyncHandler = require('express-async-handler')
+
+const getProducts =  asyncHandler(async (req,res)=>{
     try {
         const product = await Product.find({})
         res.status(200).json(product)
     }catch (error){
-        res.status(500).json({message:error.message})
+        res.status(500)
+        throw new Error(error.message)
     }
-}
+})
 //get single product
-const getProduct= async (req,res)=>{
+const getProduct=  asyncHandler(async (req,res)=>{
     try {
         const {id}= req.params;
         const product = await Product.findById(id)
         res.status(200).json(product)
     }catch (error){
-        console.log(error.message)
-        res.status(500).json({message:error.message})
+        res.status(500)
+        throw new Error(error.message)
+        // console.log(error.message)
+        // res.status(500).json({message:error.message})
     }
-}
+})
 //create a product
-const createProduct = async (req  ,res)=>{
+const createProduct = asyncHandler(async (req  ,res)=>{
     try{
         const product = await Product.create(req.body)
         res.status(200).json(product);
     } catch (error){
-        console.log(error.message);
-        res.status(500).json({message:error.message});
+        res.status(500)
+        throw new Error(error.message)
     }
-}
-const updateProduct = async (req,res)=>{
+})
+const updateProduct = asyncHandler(async (req,res)=>{
     try {
         const {id}= req.params;
         const product = await Product.findByIdAndUpdate(id,req.body,{new: true});
         if(!product){
             //     //we cannot find a product with that id
-            return res.status(404).json({message: `cannot find your id ${id}`})
+            res.status(404)
+            throw new Error(`cannot find your id ${id}`);
+            //return res.status(404).json({})
         }
         // const updatedProduct=await Product.findById(id);
         return res.status(200).json(product);
     }catch (error){
-        console.log(error)
-        res.status(500).json({message:error.message})
+        res.status(500)
+        throw new Error(error.message)
     }
-}
-const deleteProduct = async (req,res)=>{
+})
+const deleteProduct =asyncHandler( async (req,res)=>{
     try {
         const {id} = req.params
         const  product = await Product.findByIdAndDelete(id)
         if(!product)
         {
-            return res.status(404).json({message: `cant find your product id ${id}`})
+            res.status(404)
+            throw new Error( `cant find your product id ${id}`)
+            //return res.status(404).json({message: `cant find your product id ${id}`})
         }
         return res.send(200).json(product);
 
     } catch (error) {
-        console.log(error)
-        res.status(500).json({message: error.message})
+        res.status(500)
+        throw new Error(error.message)
     }
-}
+})
 module.exports = {
     getProducts,getProduct,createProduct,updateProduct,deleteProduct,
 }
--- server.js
+++ server.js
@@ -3,21 +3,32 @@ const express = require('express');
 const mongoose = require("mongoose");
 const app = express();
 const {json} = require("express");
-
+var cors = require('cors')
 app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT || 3000
+const FRONTEND = process.env.FRONTEND;
 const productRoute = require("./routes/productRoutes.JS")
 const userRoute = require("./routes/userRoutes")
+const errorMiddleware = require("./Middleware/errorMiddleware")
 //routes
+app.use(errorMiddleware);
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use('/api/products',productRoute);
 app.use('/api/users',userRoute);
-app.get('/',(req,res)=>{
-       res.send("Hello Node API")
-})
+ app.get('/',(req,res)=>{
+
+     res.send("Hello Node API")
+ })
 app.get("/blog",(req,res)=>{
      res.send('HELLO BLOGS')
 })
+
 //send data from client(client creat) to database
 
 // console.log(req.body);
